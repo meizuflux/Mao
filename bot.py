ea3fdaa655560c9a10b6e3952723fc2bc741c248
ppotatoo <rkawas731@outlook.com>
2021-04-09 07:48:03 -0400

    here goes nothing

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
-import discord
-from discord.ext import commands
 import asyncio
-import toml
 import logging
+import os
+
+import aiohttp
+import asyncpg
+import discord
+import toml
+from discord.ext import commands
 
 try:
     import uvloop
@@ -11,50 +15,52 @@ except ImportError:
 else:
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
+logger = logging.getLogger("Walrus")
+logging.basicConfig(
+    format="%(levelname)s (%(name)s) |:| %(message)s |:| %(pathname)s:%(lineno)d",
+    datefmt="%m/%d/%Y %-I:%M:%S",
+    level=logging.INFO,
+)
+
 
 class Mao(commands.Bot):
     def __init__(self, *args, **kwargs):
-      super().__init__(*args, **kwargs)
-      
-      #  core variables
-      with open("config.toml") as f:
-          self.settings = toml.loads(f.read())
-      self.embed_color = "TBD"
-      
-      #   asyncio things
-      self.loop = asyncio.get_event_loop()
-      self.db = self.loop.run_until_complete(asyncpg.create_pool(dsn=self.settings['tokens']['dsn']))
-      self.loop.create_task(self.__prep())
-      
-      
+        super().__init__(*args, **kwargs)
+
+        #  core variables
+        with open("config.toml") as f:
+            self.settings = toml.loads(f.read())
+        self.embed_color = "TBD"
+
+        #   asyncio things
+        self.loop = asyncio.get_event_loop()
+        self.db = self.loop.run_until_complete(asyncpg.create_pool(dsn=self.settings['core']['postgres_dsn']))
+        self.loop.create_task(self.__prep())
+
     async def __prep(self):
         self.session = aiohttp.ClientSession(loop=self.loop)
         with open("schema.sql") as f:
             await self.db.execute(f.read())
-          
+
     async def on_ready(self):
         logging.info("Connected to Discord.")
-        
-    def run(token: str):
+
+    def run(self, token: str):
         for file in os.listdir("exts"):
             if not file.startswith("_"):
                 self.load_extension(f'exts.{file[:-3]}')
         self.load_extension("jishaku")
-        logging.info("Loaded extensions.)
+        logger.info("Loaded extensions.")
         super().run(token=token)
-        
+
     async def close(self):
         await self.session.close()
         await self.db.close()
         await super().close()
-                     
+
     def embed(self, ctx, **kwargs):
         color = kwargs.pop("color", self.embed_color)
         embed = discord.Embed(**kwargs, color=color)
         embed.timestamp = ctx.message.created_at
         embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
         return embed
-          
-      
-      
-      
---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from bot import Mao
+import discord
+
+intents = discord.Intents.default()
+intents.members = True
+intents.integrations = False
+intents.webhooks = False
+intents.invites = False
+intents.voice_states = False
+intents.typing = False
+
+flags = discord.MemberCacheFlags.from_intents(intents)
+
+bot = Mao(
+    command_prefix="mao ",
+    case_insensitive=True,
+    intents=intents,
+    member_cache_flags=flags,
+    max_messages=750,
+    owner_ids={809587169520910346}
+)
+
+os.environ['JISHAKU_NO_UNDERSCORE'] = "True"
+os.environ['JISHAKU_NO_DM_TRACEBACK'] = "True"
+os.environ['JISHAKU_HIDE'] = "True"
+
+if __name__ == "__main__":
+    bot.run(bot.settings['core']['token'])
